98ac5c104a5207b704aed3849de9b97311f8d2ed
Milos Popovic <milos_popovic@outlook.com>
2020-08-17 02:05:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,6 +2,6 @@ class Post
   attr_accessor :title, :author
   def initialize
     @title = title
-    post.author = author
+    post.author = author  #which is actualy a created instance trough the test author = Author.new
   end
 end
