86b1c80c482c0c432c202b1b2bf12afefc3d8ec0
Milos Popovic <milos_popovic@outlook.com>
2020-08-17 02:06:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
  class Post
 #
    attr_accessor :title, :author
-#   def initialize(title)
-#     @title = title
-#
-#   end
-#
+  def initialize(title)
+    @title = title
+
+  end
+
  end
 
 # post = Post.new("post")
