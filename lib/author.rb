ccc9f428dada07ff67482a82e86f9bf25f6dd241
Milos Popovic <milos_popovic@outlook.com>
2020-08-17 02:06:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,11 @@
  class Author
 #
    attr_accessor :name
-#   def initialize(name)
-#     @name = name
-#
-#   end
-#
+  def initialize(name)
+    @name = name
+
+  end
+
  end
 
 # hillary = Author.new("Hillary")
